48b9cd0a7f3c2b6ddf74ac95898eeca2dada3552
ravikishan.69 <ravikishan63392@gmail.com>
2023-11-12 14:05:00 +0530

    Adding Syntax Highlighting

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,46 +1,90 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import rehypePrettyCode from "rehype-pretty-code";
+import { getHighlighter, BUNDLED_LANGUAGES } from 'shiki';
+import sitemap from "@astrojs/sitemap";
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: {
+    dark: "github-dark-dimmed",
+    light: "github-light",
+  },
+  keepBackground: true,
+  grid: true,
+  filterMetaString: (string) => string.replace(/filename="[^"]*"/, ""),
+//   getHighlighter: (options) =>
+//     getHighlighter({
+//       ...options,
+//       langs: [
+//         ...BUNDLED_LANGUAGES,
+//         {
+//           id: "groq",
+//           scopeName: "source.groq",
+//           path: "./langs/vscode-sanity/grammars/groq.json",
+//         },
+//       ],
+//     }),
+  onVisitLine: (line) => {
+    if (line.number === 2) {
+      return {
+        ...line,
+        className: "line-highlight",
+      };
+    }
+    return line;
+  },
+  onVisitHighlightedLine: (line) => {
+    return {
+      ...line,
+      className: "line-highlight",
+    };
+  },
+  defaultLang: "python",
+};
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Python Central Hub',
-			social: {
-				github: 'https://github.com/Ravikisha/PythonCentralHub.git',
-				instagram: 'https://www.instagram.com/ravikishan.69',
-				"x.com": 'https://twitter.com/@Ravikishan_',
-				email: 'mailto:ravikishan63392@gmail.com',
-				linkedin: 'https://www.linkedin.com/in/ravi-kishan-62ab51221/'
-			},
-			sidebar: [
-				// {
-				// 	label: 'Guides',
-				// 	items: [
-				// 		// Each item here is one entry in the navigation menu.
-				// 		{ label: 'Example Guide', link: '/guides/example/' },
-				// 	],
-				// },
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: 'tutorials' },
-				},
-				{
-					label: 'Projects',
-					autogenerate: { directory: 'projects' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: [
-				'./src/styles/global.css',
-			]
-		}),
-	],
+  markdown: {
+    syntaxHighlight: false, // Disable syntax built-in syntax hightlighting from astro
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+  integrations: [
+    starlight({
+      title: "Python Central Hub",
+      social: {
+        github: "https://github.com/Ravikisha/PythonCentralHub.git",
+        instagram: "https://www.instagram.com/ravikishan.69",
+        "x.com": "https://twitter.com/@Ravikishan_",
+        email: "mailto:ravikishan63392@gmail.com",
+        linkedin: "https://www.linkedin.com/in/ravi-kishan-62ab51221/",
+      },
+      sidebar: [
+        // {
+        // 	label: 'Guides',
+        // 	items: [
+        // 		// Each item here is one entry in the navigation menu.
+        // 		{ label: 'Example Guide', link: '/guides/example/' },
+        // 	],
+        // },
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Tutorials",
+          autogenerate: { directory: "tutorials" },
+        },
+        {
+          label: "Projects",
+          autogenerate: { directory: "projects" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/global.css"],
+    }),
+  ],
 });
--- docs/src/content/docs/guides/example.md
+++ docs/src/content/docs/guides/example.md
@@ -9,3 +9,23 @@ Writing a good guide requires thinking about what your users are trying to do.
 ## Further reading
 
 - Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
+```python title="somethingComponent.ts" showLineNumbers{1}
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+
+app = Starlette(debug=True)
+app.add_route("/", homepage)
+app.add_route("/items/{item_id}", item_detail) ==> lkjsddddddddddddddddddddddddddddddddddddddddddddddahjdfhlaksjdfahlsjdjfhlaskdjfhlskjdh
+```
+
+```js title="somethingComponent.ts" {1,3,5-6} showLineNumbers{1}
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+const CodeBlock = ({ codestring }) => {
+  return (
+    <SyntaxHighlighter language="javascript" style={docco}>
+            {codeString}
+    </SyntaxHighlighter>
+  );
+};
+```
