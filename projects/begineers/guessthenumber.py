cdd9745e4981fa6a5dc3d1adc671e6a07eac49c0
ravikishan.69 <ravikishan63392@gmail.com>
2023-12-08 09:41:57 +0530

    Guess the Number Program

---
+++ projects/begineers/guessthenumber.py
@@ -0,0 +1,25 @@
+# Guess the number game
+
+import random
+
+print('Hello, what is your name?')
+name = input()
+
+print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
+secretNumber = random.randint(1, 20)
+
+for guessesTaken in range(1, 7):
+    print('Take a guess. You have ' + str(7 - guessesTaken) + ' guesses left.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else: 
+        break # This condition is the correct guess!
+    
+if guess == secretNumber:
+    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses.')
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber) + '.')
