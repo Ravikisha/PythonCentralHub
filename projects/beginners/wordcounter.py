14a974d2d28c6bfe76dc18b0994c785be2118e12
ravikishan.69 <ravikishan63392@gmail.com>
2023-12-23 22:16:32 +0530

    Adding Another Way to solve the Word Counter

--- projects/beginners/wordcounter.py
+++ projects/beginners/wordcounter.py
@@ -20,3 +20,25 @@ print(counter.most_common(10))
 
 # Close the file
 text.close()
+
+
+# Alternative solution
+words2 = {}
+
+# Loop through the list of words
+for word in words:
+    # If the word is not in the dictionary, add it
+    if word not in words2:
+        words2[word] = 1
+    # If the word is in the dictionary, increment its value
+    else:
+        words2[word] += 1
+        
+# Sort the dictionary by value in descending order
+sorted_words = sorted(words2.items(), key=lambda x: x[1], reverse=True)
+
+# Print the 10 most common words
+print(sorted_words[:10])
+
+# Close the file
+text.close()
