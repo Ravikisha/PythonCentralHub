834cb2afe55ea82a9c8cb4cec54ed57c384c2492
ravikishan.69 <ravikishan63392@gmail.com>
2023-12-25 21:47:28 +0530

    Adding Simple Reminder App for Mac OS

--- projects/beginners/simplereminderapp.py
+++ projects/beginners/simplereminderapp.py
@@ -1,5 +1,6 @@
 # Simple Reminder App
 
+# Reminder App For Windows
 # Import Modules
 import time
 import datetime
@@ -48,3 +49,49 @@ def intro():
     
 # Calling Functions
 intro()
+
+
+# Reminder App For Mac
+# import time
+# import datetime
+# import pync
+
+# # Defining Functions
+# def print_pause(message_to_print):
+#     print(message_to_print)
+#     time.sleep(2)
+    
+# def reminder():
+#     print_pause("What would you like to be reminded about?")
+#     reminder = input("Enter your reminder: ")
+#     print_pause("In how many minutes would you like to be reminded?")
+#     minutes = input("Enter minutes: ")
+#     print_pause("Reminder set!")
+#     time.sleep(int(minutes) * 60)
+#     pync.notify(reminder, title="Reminder", sound="default")
+#     print_pause("Reminder: " + reminder)
+#     print_pause("Reminder Completed!")
+#     play_again()
+    
+# def play_again():
+#     print_pause("Would you like to set another reminder?")
+#     while True:
+#         choice = input("Enter yes or no: ")
+#         if choice == "yes":
+#             print_pause("Great!")
+#             reminder()
+#             break
+#         elif choice == "no":
+#             print_pause("Thank you for using the Reminder App!")
+#             break
+#         else:
+#             print_pause("Please enter yes or no: ")
+            
+# def intro():
+#     print_pause("Welcome to the Reminder App!")
+#     print_pause("This app will remind you about anything you want!")
+#     print_pause("Let's get started!")
+#     reminder()
+    
+# # Calling Functions
+# intro()
